37c876a054e01a7e17ce3597dbfe059a9e93c5eb
Harish-lodh <harishlodh444@gmail.com>
2024-07-02 18:34:44 +0530

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -19,7 +19,7 @@ const Reservation = () => {
   
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://restaurant-mern-stack-project.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
   
       );
